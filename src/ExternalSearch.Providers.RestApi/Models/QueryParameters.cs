b99b704f579c21c2248e271d402ab3f311bd2575
Yi-Wei <ywei390@gmail.com>
2025-04-28 10:10:09 +0800

    chore: Include all configs into query parameters

---
+++ src/ExternalSearch.Providers.RestApi/Models/QueryParameters.cs
@@ -0,0 +1,13 @@
+﻿namespace CluedIn.ExternalSearch.Providers.RestApi.Models;
+
+public class QueryParameters
+{
+    public string Url { get; set; }
+    public string Method { get; set; }
+    public string Body { get; set; }
+    public string ApiKey { get; set; }
+    public string Headers { get; set; }
+    public string ProcessRequestScript { get; set; }
+    public string ProcessResponseScript { get; set; }
+}
+
